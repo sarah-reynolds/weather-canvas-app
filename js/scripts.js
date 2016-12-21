a7578ab32e087a17a866f935156d1f63664e49dd
Sarah Basinger <sarrey@gmail.com>
2016-12-20 21:36:55 -0500

    functioning, with temp, condition, wind, humidity. added styles

--- js/scripts.js
+++ js/scripts.js
@@ -8,34 +8,39 @@ $(document).ready(function(){
 		$.getJSON(weatherUrl, function(weatherData){
 		// console.log(weatherData);
 			var currTemp = weatherData.main.temp;
+			var currHumid = weatherData.main.humidity;
 			var currTempText = Math.floor(currTemp)+'\xB0';
 			// console.log(currTemp)
 			var name = weatherData.name;
+			$('#condition').html(weatherData.weather[0].description);
+			$('#wind').html(weatherData.wind.speed+"mph");
+			$('#humidity').html(weatherData.main.humidity+"%");
 			var icon = weatherData.weather[0].icon+'.png';
-			$('#currTemp').html("<img src=http://openweathermap.org/img/w/"+icon+">The temp in "+name+" is currently:");
+			$('#currTemp').html("<img src=http://openweathermap.org/img/w/"+icon+">The temperature in "+name+" is currently:");
 			var canvas = $('#weather-canvas');
 			var context = canvas[0].getContext('2d');
 			var tempColor = "black";
 			if(currTemp < 32){
 				tempColor = "skyblue";
 			}else if(currTemp < 60){
-				tempColor = "blue";
+				tempColor = "steelblue";
 			}else if(currTemp < 80){
 				tempColor = "orange";
-			}else if(currTemp > 80){
-				tempColor = "red";
+			}else if(currTemp > 79){
+				tempColor = "tomato";
 			}
 
 			var currPercent = 0;
 			function animate(current){
 				// draw inner circle
-				
+				$('#weather-canvas').addClass('hide-bg')
+				context.clearRect(0,0,500,500)
 				context.fillStyle = "white";
 				context.beginPath();
 				context.arc(155,75,65,Math.PI*0,Math.PI*2);
 				context.closePath();
 				context.fill();
-				console.log(weatherData)
+				// console.log(weatherData)
 
 				context.font = "50px Arial"
 				context.fillStyle = tempColor;
@@ -56,6 +61,7 @@ $(document).ready(function(){
 				}
 			}
 			animate();
+			console.log(weatherData);
 		});
 	})
 });
